2a8943db39cbdbb4b049d5286d2ae4513eabe70b
carrexxii <cjbarrett101@gmail.com>
2024-10-27 02:30:34 +0200

    Add messagebox

---
+++ sdl/msgbox.nim
@@ -0,0 +1,48 @@
+import bitgen
+
+type
+    MessageBoxFlag* {.size: sizeof(uint32).} = enum
+        msgBoxError       = 0x0010
+        msgBoxWarning     = 0x0020
+        msgBoxInformation = 0x0040
+        msgBoxButtonsLtr  = 0x0080
+        msgBoxButtonsRtl  = 0x0100
+
+    MessageBoxButtonFlag* {.size: sizeof(uint32).} = enum
+        msgBoxButtonReturnKeyDefault = 1
+        msgBoxButtonEscapeKeyDefault = 2
+
+    MessageBoxColourKind* {.size: sizeof(cint).} = enum
+        msgBoxColourBackground
+        msgBoxColourText
+        msgBoxColourButtonBorder
+        msgBoxColourButtonbackground
+        msgBoxColourButtonSelected
+
+type
+    MessageBoxButtonData* = object
+        flags*: MessageBoxButtonFlag
+        id*   : cint
+        text* : cstring
+
+    MessageBoxColour* = object
+        r*: uint8
+        g*: uint8
+        b*: uint8
+
+    MessageBoxColourScheme* = object
+        colours*: array[1 + high MessageBoxColourKind, MessageBoxColour]
+
+    MessageBoxData* = object
+        flags*        : MessageBoxFlag
+        win*          : Window
+        title*        : cstring
+        msg*          : cstring
+        btn_count*    : cint
+        btns*         : ptr UncheckedArray[MessageBoxButtonData]
+        colour_scheme*: ptr MessageBoxColourScheme
+
+{.push dynlib: SdlLib.}
+proc show_message_box*(data: ptr MessageBoxData; btn_id: ptr cint): cbool                     {.importc: "SDL_ShowMessageBox"      .}
+proc show_simple_message_box*(flags: MessageBoxFlag; title, msg: cstring; win: Window): cbool {.importc: "SDL_ShowSimpleMessageBox".}
+{.pop.}
--- sdl/rect.nim
+++ sdl/rect.nim
@@ -2,106 +2,34 @@ import common
 
 type
     Point* = object
-        x*, y*: int32 = 0
+        x*, y*: cint = 0
     FPoint* = object
         x*, y*: float32 = 0.0
 
     Rect* = object
-        x*, y*, w*, h*: int32 = 0
+        x*, y*, w*, h*: cint = 0
     FRect* = object
         x*, y*, w*, h*: float32 = 0.0
 
 {.push inline.}
 
-func point*(x, y: SomeNumber = 0): Point =
-    result = Point(x: int32 x, y: int32 y)
-func fpoint*(x, y: SomeNumber = 0.0): FPoint =
-    result = FPoint(x: float32 x, y: float32 y)
+template point*(x, y: SomeNumber = 0): Point     =  Point(x: int32   x, y: int32   y)
+template fpoint*(x, y: SomeNumber = 0.0): FPoint = FPoint(x: float32 x, y: float32 y)
 
-func rect*(x, y, w, h: SomeNumber = 0): Rect =
-    result = Rect(x: int32 x, y: int32 y, w: int32 w, h: int32 h)
-func frect*(x, y, w, h: SomeNumber = 0.0): FRect =
-    result = FRect(x: float32 x, y: float32 y, w: float32 w, h: float32 h)
-func rect*(rect: FRect): Rect  = rect( rect.x, rect.y, rect.w, rect.h)
-func frect*(rect: Rect): FRect = frect(rect.x, rect.y, rect.w, rect.h)
+template rect*(x, y, w, h: SomeNumber = 0): Rect     =  Rect(x: cint    x, y: cint    y, w: cint    w, h: cint    h)
+template frect*(x, y, w, h: SomeNumber = 0.0): FRect = FRect(x: float32 x, y: float32 y, w: float32 w, h: float32 h)
 
-func in_rect*(pt: Point | FPoint; rect: Rect | FRect): bool =
-    (pt.x >= rect.x) and (pt.x < rect.x + rect.w) and
-    (pt.y >= rect.y) and (pt.y < rect.y + rect.h)
-func `in`*(pt: Point | FPoint          ; rect: Rect | FRect): bool = pt.in_rect rect
-func `in`*(pt: (SomeNumber, SomeNumber); rect: Rect)        : bool =  point(pt[0], pt[1]).in_rect rect
-func `in`*(pt: (SomeNumber, SomeNumber); rect: FRect)       : bool = fpoint(pt[0], pt[1]).in_rect rect
-func `notin`*(pt: (SomeNumber, SomeNumber) | Point | FPoint; rect: Rect | FRect): bool = not (pt in rect)
+func rect*(r: FRect): Rect  = rect  r.x, r.y, r.w, r.h
+func frect*(r: Rect): FRect = frect r.x, r.y, r.w, r.h
+
+func in_rect*(pt: Point | FPoint; r: Rect | FRect): bool =
+    (pt.x >= r.x) and (pt.x < r.x + r.w) and
+    (pt.y >= r.y) and (pt.y < r.y + r.h)
+func `in`*(pt: Point | FPoint; r: Rect | FRect): bool    = pt.in_rect r
+func `in`*(pt: (SomeNumber, SomeNumber); r: Rect): bool  =  point(pt[0], pt[1]).in_rect r
+func `in`*(pt: (SomeNumber, SomeNumber); r: FRect): bool = fpoint(pt[0], pt[1]).in_rect r
+func `notin`*(pt: (SomeNumber, SomeNumber) | Point | FPoint; r: Rect | FRect): bool = not (pt in r)
+
+func empty*(r: Rect | FRect): bool = (r.w <= 0) or (r.h <= 0)
 
 {.pop.}
-
-# TODO
-
-# SDL_FORCE_INLINE SDL_bool SDL_PointInRect(const SDL_Point *p, const SDL_Rect *r)
-# {
-#     return ( p && r && (p->x >= r->x) && (p->x < (r->x + r->w)) &&
-#              (p->y >= r->y) && (p->y < (r->y + r->h)) ) ? SDL_TRUE : SDL_FALSE;
-# }
-# SDL_FORCE_INLINE SDL_bool SDL_RectEmpty(const SDL_Rect *r)
-# {
-#     return ((!r) || (r->w <= 0) || (r->h <= 0)) ? SDL_TRUE : SDL_FALSE;
-# }
-# SDL_FORCE_INLINE SDL_bool SDL_RectsEqual(const SDL_Rect *a, const SDL_Rect *b)
-# {
-#     return (a && b && (a->x == b->x) && (a->y == b->y) &&
-#             (a->w == b->w) && (a->h == b->h)) ? SDL_TRUE : SDL_FALSE;
-# }
-# extern DECLSPEC SDL_bool SDLCALL SDL_HasRectIntersection(const SDL_Rect * A,
-#                                                          const SDL_Rect * B);
-# extern DECLSPEC SDL_bool SDLCALL SDL_GetRectIntersection(const SDL_Rect * A,
-                                                #    const SDL_Rect * B,
-                                                #    SDL_Rect * result);
-# extern DECLSPEC int SDLCALL SDL_GetRectUnion(const SDL_Rect * A,
-                                        #    const SDL_Rect * B,
-                                        #    SDL_Rect * result);
-# extern DECLSPEC SDL_bool SDLCALL SDL_GetRectEnclosingPoints(const SDL_Point * points,
-#                                                    int count,
-#                                                    const SDL_Rect * clip,
-#                                                    SDL_Rect * result);
-# extern DECLSPEC SDL_bool SDLCALL SDL_GetRectAndLineIntersection(const SDL_Rect *
-                                                        #   rect, int *X1,
-                                                        #   int *Y1, int *X2,
-                                                        #   int *Y2);
-# SDL_FORCE_INLINE SDL_bool SDL_PointInRectFloat(const SDL_FPoint *p, const SDL_FRect *r)
-# {
-#     return ( p && r && (p->x >= r->x) && (p->x < (r->x + r->w)) &&
-#              (p->y >= r->y) && (p->y < (r->y + r->h)) ) ? SDL_TRUE : SDL_FALSE;
-# }
-# SDL_FORCE_INLINE SDL_bool SDL_RectEmptyFloat(const SDL_FRect *r)
-# {
-#     return ((!r) || (r->w <= 0.0f) || (r->h <= 0.0f)) ? SDL_TRUE : SDL_FALSE;
-# }
-# SDL_FORCE_INLINE SDL_bool SDL_RectsEqualEpsilon(const SDL_FRect *a, const SDL_FRect *b, const float epsilon)
-# {
-#     return (a && b && ((a == b) ||
-#             ((SDL_fabsf(a->x - b->x) <= epsilon) &&
-#             (SDL_fabsf(a->y - b->y) <= epsilon) &&
-#             (SDL_fabsf(a->w - b->w) <= epsilon) &&
-#             (SDL_fabsf(a->h - b->h) <= epsilon))))
-#             ? SDL_TRUE : SDL_FALSE;
-# }
-# SDL_FORCE_INLINE SDL_bool SDL_RectsEqualFloat(const SDL_FRect *a, const SDL_FRect *b)
-# {
-#     return SDL_RectsEqualEpsilon(a, b, SDL_FLT_EPSILON);
-# }
-# extern DECLSPEC SDL_bool SDLCALL SDL_HasRectIntersectionFloat(const SDL_FRect * A,
-#                                                       const SDL_FRect * B);
-# extern DECLSPEC SDL_bool SDLCALL SDL_GetRectIntersectionFloat(const SDL_FRect * A,
-                                                    # const SDL_FRect * B,
-                                                    # SDL_FRect * result);
-# extern DECLSPEC int SDLCALL SDL_GetRectUnionFloat(const SDL_FRect * A,
-                                            # const SDL_FRect * B,
-                                            # SDL_FRect * result);
-# extern DECLSPEC SDL_bool SDLCALL SDL_GetRectEnclosingPointsFloat(const SDL_FPoint * points,
-                                                    # int count,
-                                                    # const SDL_FRect * clip,
-                                                    # SDL_FRect * result);
-# extern DECLSPEC SDL_bool SDLCALL SDL_GetRectAndLineIntersectionFloat(const SDL_FRect *
-                                                        #    rect, float *X1,
-                                                        #    float *Y1, float *X2,
-                                                        #    float *Y2);
